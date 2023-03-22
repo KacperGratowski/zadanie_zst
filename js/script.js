69de30d5d38781f81e349dbf93022e531c5ffb68
KacperGratowski <111381308+KacperGratowski@users.noreply.github.com>
2023-03-22 15:50:48 +0100

    Sum shit

---
+++ js/script.js
@@ -0,0 +1,274 @@
+	const mus = ["mp3/mlben.mp3","mp3/mlbfr.mp3","mp3/mlbpl.mp3"];
+function ranomize(){
+	var licz = Math.floor(Math.random() * 3);
+	return (licz);
+	
+}
+function miraculous(){
+	var value = ranomize();
+	var audio = new Audio(mus[value]);
+	
+	audio.play();
+	if (value==0) {
+		alert("In the daytime")
+		alert("I'm Marinette")
+		alert("Just a normal girl")
+		alert("With a normal life")
+		alert("But there's something about me") 
+		alert("That no one knows yet") 
+		alert("Cause I have a secret") 
+		alert("Another day")
+		alert("I'm back at school")
+		alert("I think about him")
+		alert("He's so cool")
+		alert("He looks at me")
+		alert("I look away")
+		alert("But does he see me anyway?")
+		alert("Uh")
+		alert("uh")
+		alert("oh")
+		alert("He's got me spinning round")
+		alert("Uh")
+		alert("uh")
+		alert("oh")
+		alert("My feet are off the ground")
+		alert("Uh")
+		alert("uh")
+		alert("oh")
+		alert("And when the sun goes down")
+		alert("That's when I become")
+		alert("Miraculous!")
+		alert("Simply the best!")
+		alert("Up to the test when things go wrong!")
+		alert("Miraculous!")
+		alert("The luckiest!")
+		alert("The power of love always so strong!")
+		alert("I'm the Cat")
+		alert("Just chilling out")
+		alert("But in the night")
+		alert("She's all I think about")
+		alert("I feel so strong")
+		alert("When she's around")
+		alert("She pick me up when")
+		alert("I'm down")
+		alert("Uh")
+		alert("uh")
+		alert("oh")
+		alert("Oh no you'll never know")
+		alert("Uh")
+		alert("uh")
+		alert("oh")
+		alert("My love can only grow")
+		alert("Uh")
+		alert("uh")
+		alert("oh")
+		alert("And when I see her smile")
+		alert("That's when she becomes")
+		alert("Miraculous!")
+		alert("Simply the best!")
+		alert("Up to the test when things go wrong!")
+		alert("Miraculous!")
+		alert("The luckiest!")
+		alert("The power of love always so strong!")
+		alert("Miraculous!")
+		alert("Simply the best!")
+		alert("Up to the test when things go wrong!")
+		alert("Miraculous!")
+		alert("The luckiest!")
+		alert("The power of love always so strong!")
+		alert("I showed the world")
+		alert("that I'm strong")
+		alert("But all I want")
+		alert("is to be alone")
+		alert("I have the gift to put away")
+		alert("Whatever is wrong and save the day")
+		alert("Uh")
+		alert("uh")
+		alert("oh")
+		alert("Oh no you'll never know")
+		alert("Uh")
+		alert("uh")
+		alert("oh")
+		alert("My love can only grow")
+		alert("Uh")
+		alert("uh")
+		alert("oh")
+		alert("And when I see her smile")
+		alert("That's when she becomes")
+		alert("Miraculous! (Miraculous)")
+		alert("Simply the best!")
+		alert("Up to the test when things go wrong!")
+		alert("Miraculous! (Uh ooh)")
+		alert("The luckiest!")
+		alert("The power of love always so strong!")
+		alert("Miraculous! (Miraculous, miraculous)")
+		alert("Miraculous! The luckiest! (The luckiest)")
+		alert("The power of love always so strong!")
+		alert("Miraculous!")
+	} 
+	else if (value==2) {
+		alert ("Tak na co dzień, jestem Marinette.");
+		alert ("Zwyczajna dziewczyna ze zwyczajnym życiem");
+		alert ("Ale skrywam w sobie coś, o czym nie wie nikt.");
+		alert ("To moja tajemnica.");
+		alert ("Każdego dnia do szkoły gnam");
+		alert ("bo tam jest on cudowny tak");
+		alert ("gdy spojrzy ja wnet chowam się");
+		alert ("co o mnie myśli kto to wie");
+		alert ("o");
+		alert ("o");
+		alert ("o");
+		alert ("zakręcił w głowie mi");
+		alert ("o");
+		alert ("o");
+		alert ("o");
+		alert ("on chyba mi się śni");
+		alert ("o");
+		alert ("o");
+		alert ("o");
+		alert ("a gdy ktoś kłopot ma");
+		alert ("zmieniam się raz dwa");
+		alert ("Mam super dar");
+		alert ("gaśnie zły czar");
+		alert ("tak działa moc miraculum");
+		alert ("Mam super dar a w sercu żar");
+		alert ("dodaje mi sił miłości duch");
+		alert ("To ja ten kot");
+		alert ("co wielbi ją");
+		alert ("lecz wszystkie me");
+		alert ("starania na nic są");
+		alert ("gdy blisko jest na sercu lżej");
+		alert ("tak wiele już zawdzięczam jej");
+		alert ("o");
+		alert ("o");
+		alert ("o");
+		alert ("miłość prowadzi mnie");
+		alert ("o");
+		alert ("o");
+		alert ("o");
+		alert ("zawsze być przy niej chcę");
+		alert ("o");
+		alert ("o");
+		alert ("o");
+		alert ("niech szczęście dłużej trwa");
+		alert ("zmieniaj się raz dwa");
+		alert ("Mam super dar");
+		alert ("gaśnie zły czar");
+		alert ("tak działa moc miraculum ");
+		alert ("Mam super dar");
+		alert ("a w sercu żar");
+		alert ("dodaje mi sił miłości duch");
+		alert ("Mam super dar");
+		alert ("gaśnie zły czar");
+		alert ("tak działa moc miraculum ");
+		alert ("Mam super dar");
+		alert ("a w sercu żar");
+		alert ("dodaje mi sił miłości duch");
+		alert ("Mam super dar");
+		alert ("gaśnie zły czar");
+		alert ("tak działa moc miraculum ");
+		alert ("Mam super dar");
+		alert ("a w sercu żar");
+		alert ("dodaje mi sił miłości duch");
+		alert ("Mam super dar");
+	}
+	else if (value==1){
+		alert("Je m'appelle Marinette,")
+		alert("Une fille comme les autres")
+		alert("Mais quand le destin me choisit pour lutter contre les forces du mal,")
+		alert("Je deviens")
+		alert("Miraculous Ladybug !")
+		alert("Papillon noir")
+		alert("Paris mystère")
+		alert("C'est mon histoire")
+		alert("Plutôt étrange")
+		alert("La magie noire")
+		alert("Me désespère")
+		alert("Et le chat noir")
+		alert("N'est pas un ange")
+		alert("ah")
+		alert("ah")
+		alert("ah")
+		alert("Amour chassé-croisé")
+		alert("ah")
+		alert("ah")
+		alert("ah")
+		alert("Mon cœur aime Adrien")
+		alert("ah")
+		alert("ah")
+		alert("ah")
+		alert("Pas ce héros masqué")
+		alert("Qui m'aime quand je deviens")
+		alert("Miraculous")
+		alert("Porte-bonheur")
+		alert("Lady Magique et Lady Chance")
+		alert("Miraculous")
+		alert("Lady du cœur")
+		alert("Être héroïque en cas d'urgence")
+		alert("C'est moi, Chat Noir")
+		alert("Toujours présent")
+		alert("J'ai des pouvoirs")
+		alert("Super puissants")
+		alert("Pour la victoire")
+		alert("J'en fais serment")
+		alert("Je me bagarre")
+		alert("Éperdument")
+		alert("ah")
+		alert("ah")
+		alert("ah")
+		alert("Amour chassé-croisé")
+		alert("ah")
+		alert("ah")
+		alert("ah")
+		alert("J'ai peur d'aimer pour rien") 
+		alert("ah")
+		alert("ah")
+		alert("ah")
+		alert("Celle qui ne peut m'aimer")
+		alert("Quand elle vit son destin")
+		alert("Miraculous")
+		alert("Porte-bonheur")
+		alert("Lady Magique et Lady Chance")
+		alert("Miraculous")
+		alert("Lady du cœur")
+		alert("Être héroïque en cas d'urgence")
+		alert("Miraculous")
+		alert("Porte-bonheur")
+		alert("Lady Magique et Lady Chance")
+		alert("Miraculous")
+		alert("Lady du cœur")
+		alert("Être héroïque en cas d'urgence")
+		alert("Dans la vraie vie")
+		alert("Je suis fragile")
+		alert("J'apprends souvent")
+		alert("À mes dépends")
+		alert("Et je souris")
+		alert("Même à la ville")
+		alert("En me jouant")
+		alert("De mes tourments")
+		alert("ah")
+		alert("ah")
+		alert("ah")
+		alert("Amour chassé-croisé")
+		alert("ah")
+		alert("ah")
+		alert("ah")
+		alert("J'ai peur d'aimer pour rien")
+		alert("ah")
+		alert("ah")
+		alert("ah")
+		alert("Celle qui ne peut m'aimer")
+		alert("Quand elle vit son destin")
+		alert("Miraculous")
+		alert("Porte-bonheur")
+		alert("Lady Magique et Lady Chance")
+		alert("Miraculous")
+		alert("Lady du cœur")
+		alert("Être héroïque en cas d'urgence")
+		alert("Miraculous (miraculous, miraculous)")
+		alert("Miraculous")
+		alert("Lady du cœur")
+		alert("Être héroïque en cas d'urgence")
+		alert("Miraculous")
+		}
+}
